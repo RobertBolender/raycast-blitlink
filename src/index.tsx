b2b19d2dc40bf4c45c04ea14ee53810845c7d7c5
Robert Bolender <robert@bolender.email>
2022-06-30 17:03:00 -0700

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import { ActionPanel, Action, Clipboard, Detail, List, showToast, Toast, environment, useNavigation } from "@raycast/api";
+import { ActionPanel, Action, Clipboard, Detail, Form, List, showToast, Toast, environment, useNavigation, popToRoot } from "@raycast/api";
 import { useState, useEffect, useRef, useCallback } from "react";
 import path from "path";
 import { promisify } from "util";
+import { isDataView } from "util/types";
 
 const exec = promisify(require('child_process').exec);
+const BLITLINK_PATH = "~/code/go-blitlink/go-blitlink";
 const LINK_FILE_NAME = "blitlinks.db";
 
 export default function Command() {
@@ -13,10 +15,9 @@ export default function Command() {
       isLoading={state.isLoading}
       onSearchTextChange={search}
       searchBarPlaceholder="Search for a saved link..."
-      enableFiltering
     >
       {state.results.map((searchResult) => (
-        <SearchListItem key={searchResult.title} searchResult={searchResult} />
+        <SearchListItem key={searchResult.id} searchResult={searchResult} />
       ))}
       {!state.searchText && state.results.length === 0 && (
         <List.EmptyView title="Type or paste your first link to get started!" icon="BlitLink.png" />
@@ -24,7 +25,7 @@ export default function Command() {
       {!!state.searchText && state.results.length === 0 && (
         <List.EmptyView title="No matches found" description="Save as a new link?" actions={
           <ActionPanel>
-            <Action title="Save link" onAction={() => appendLink(state.searchText ?? "")} />
+            <Action.Push title="Save link" target={<EditForm text={state.searchText ?? ""} />} />
           </ActionPanel>
         } />
       )
@@ -37,23 +38,27 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   const { push } = useNavigation();
   return (
     <List.Item
-      title={searchResult.title}
-      keywords={searchResult.keywords}
-      detail={<List.Item.Detail markdown={`![](${searchResult.url})\n\nKeywords:\n${searchResult.keywords?.join(' ')}`} />}
+      title={searchResult.text ?? ""}
+      detail={<List.Item.Detail markdown={`![](${searchResult.link})\n\n<b>${searchResult.shortcut}</b>\n${searchResult.title}`} />}
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <Action.OpenInBrowser title="Open in Browser" url={searchResult.url} />
-            <Action title="Copy link and Preview Image" onAction={() => previewAndCopy(push, searchResult)} />
+            {searchResult.link && (
+              <>
+                <Action.OpenInBrowser title="Open in Browser" url={searchResult.link} />
+                <Action title="Copy link and Preview Image" onAction={() => previewAndCopy(push, searchResult)} />
+              </>
+            )}
+            <Action.Push title="Edit link" target={<EditForm {...searchResult} />} />
           </ActionPanel.Section>
           <ActionPanel.Section>
             <Action.CopyToClipboard
               title="Copy plain URL"
-              content={`${searchResult.url}`}
+              content={`${searchResult.link}`}
             />
             <Action.CopyToClipboard
               title="Copy markdown link"
-              content={`[${searchResult.title}](${searchResult.url})`}
+              content={`[${searchResult.title}](${searchResult.link})`}
             />
           </ActionPanel.Section>
         </ActionPanel>
@@ -62,10 +67,41 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
   );
 }
 
+function EditForm(props: { text?: string, link?: string, title?: string, shortcut?: string }) {
+  const { text, link, title, shortcut } = props;
+
+  const { pop } = useNavigation();
+
+  async function handleSubmit(values: SearchResult) {
+    const { text = "", link = "", title = "", shortcut = "" } = values;
+    console.log({ values })
+    await appendLink(text, link, title, shortcut);
+    pop();
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm
+            title="Save link"
+            onSubmit={handleSubmit}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField id="text" title="Text" value={text ?? ""} />
+      <Form.TextField id="link" title="Link" value={link ?? ""} />
+      <Form.TextField id="title" title="Title" value={title ?? ""} />
+      <Form.TextField id="shortcut" title="Shortcut" value={shortcut ?? ""} />
+    </Form>
+  )
+}
+
 function previewAndCopy(push: (view: JSX.Element) => void, searchResult: SearchResult) {
-  Clipboard.copy(searchResult.url);
+  Clipboard.copy(searchResult.link!);
   showToast({ style: Toast.Style.Success, title: "Copied link to clipboard" });
-  push(<Detail markdown={`![](${searchResult.url})`} />)
+  push(<Detail markdown={`![](${searchResult.link})`} />)
 }
 
 function useSearch() {
@@ -83,7 +119,7 @@ function useSearch() {
       }));
 
       try {
-        const results = await performSearch();
+        const results = await performSearch(searchText);
         setState(() => ({
           results,
           searchText,
@@ -115,14 +151,20 @@ function useSearch() {
   };
 }
 
-async function appendLink(link: string) {
-  showToast({ style: Toast.Style.Failure, title: "Not yet implemented" });
+async function appendLink(text: string, link: string, title: string, shortcut: string) {
+  const results = await exec(`${BLITLINK_PATH} "${getLinkFileName()}" insert "${text}" "${link}" "${title}" "${shortcut}"`);
+  console.log({ results })
+  showToast({ style: Toast.Style.Success, title: "Link saved" });
 }
 
-async function performSearch(): Promise<any> {
-  const results = await exec(`~/code/go-blitlink/go-blitlink "${getLinkFileName()}" query github`);
+async function performSearch(query: string): Promise<any> {
+  const results = await exec(`${BLITLINK_PATH} "${getLinkFileName()}" query t`);
   const json = JSON.parse(results.stdout);
-  return json;
+  const items = json.map((item: any) => {
+    const [id, text, link, title, shortcut] = item;
+    return { id, text, link, title, shortcut }
+  });
+  return items;
 }
 
 function getLinkFileName() {
@@ -136,7 +178,9 @@ interface SearchState {
 }
 
 interface SearchResult {
-  title: string;
-  keywords?: string[];
-  url: string;
+  id?: string;
+  text?: string;
+  link?: string;
+  title?: string;
+  shortcut?: string;
 }
