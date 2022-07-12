ac60989ee058f8bdc7770a985c985e87b3aa5d17
Robert Bolender <robert@bolender.email>
2022-07-11 22:53:36 -0700

    Run prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,16 @@
-import { ActionPanel, Action, Clipboard, Detail, Form, Icon, List, showToast, Toast, environment, useNavigation } from "@raycast/api";
+import {
+  ActionPanel,
+  Action,
+  Clipboard,
+  Detail,
+  Form,
+  Icon,
+  List,
+  showToast,
+  Toast,
+  environment,
+  useNavigation,
+} from "@raycast/api";
 import { useState, useEffect, useRef, useCallback } from "react";
 import path from "path";
 import { promisify } from "util";
@@ -10,7 +22,9 @@ const LINK_FILE_NAME = "blitlinks.db";
 function useSqliteDatabase() {
   useEffect(() => {
     async function init() {
-      await exec(`sqlite3 "${getLinkFileName()}" "create virtual table if not exists blitlinks using fts5(text, link, title, shortcut);"`);
+      await exec(
+        `sqlite3 "${getLinkFileName()}" "create virtual table if not exists blitlinks using fts5(text, link, title, shortcut);"`
+      );
     }
     init();
   }, []);
@@ -20,11 +34,7 @@ export default function Command() {
   useSqliteDatabase();
   const { state, search } = useSearch();
   return (
-    <List
-      isLoading={state.isLoading}
-      onSearchTextChange={search}
-      searchBarPlaceholder="Search for a saved link..."
-    >
+    <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search for a saved link...">
       {state.results.map((searchResult) => (
         <SearchListItem key={searchResult.rowid} searchResult={searchResult} onEdit={search} />
       ))}
@@ -32,18 +42,21 @@ export default function Command() {
         <List.EmptyView title="Type or paste any text to get started" icon="BlitLink.png" />
       )}
       {!!state.searchText && state.results.length === 0 && (
-        <List.EmptyView title="No matches found" description="Save as a new link?" actions={
-          <ActionPanel>
-            <Action.Push
-              icon={Icon.Plus}
-              title="Save link"
-              target={<EditForm text={state.searchText ?? ""} onEdit={search} />}
-              shortcut={{ modifiers: [], key: "tab" }}
-            />
-          </ActionPanel>
-        } />
-      )
-      }
+        <List.EmptyView
+          title="No matches found"
+          description="Save as a new link?"
+          actions={
+            <ActionPanel>
+              <Action.Push
+                icon={Icon.Plus}
+                title="Save link"
+                target={<EditForm text={state.searchText ?? ""} onEdit={search} />}
+                shortcut={{ modifiers: [], key: "tab" }}
+              />
+            </ActionPanel>
+          }
+        />
+      )}
     </List>
   );
 }
@@ -52,23 +65,29 @@ function looksLikeAnImage(text: string) {
   return text.match(/\.(jpg|jpeg|png|gif|bmp|webp|svg)$/i);
 }
 
-function SearchListItem({ searchResult, onEdit }: { searchResult: SearchResult, onEdit: (text?: string) => void }) {
+function SearchListItem({ searchResult, onEdit }: { searchResult: SearchResult; onEdit: (text?: string) => void }) {
   const { push } = useNavigation();
   return (
     <List.Item
       title={searchResult.text ?? ""}
       subtitle={searchResult.link}
       accessories={[{ text: searchResult.shortcut }]}
-      detail={<List.Item.Detail markdown={`![](${searchResult.link})\n\n<b>${searchResult.shortcut}</b>\n${searchResult.title}`} />}
+      detail={
+        <List.Item.Detail
+          markdown={`![](${searchResult.link})\n\n<b>${searchResult.shortcut}</b>\n${searchResult.title}`}
+        />
+      }
       actions={
         <ActionPanel>
           <ActionPanel.Section>
             {searchResult.link && looksLikeAnImage(searchResult.link) && (
-              <Action icon={Icon.Clipboard} title="Copy link and Preview Image" onAction={() => previewAndCopy(push, searchResult)} />
+              <Action
+                icon={Icon.Clipboard}
+                title="Copy link and Preview Image"
+                onAction={() => previewAndCopy(push, searchResult)}
+              />
             )}
-            {searchResult.link && (
-              <Action.OpenInBrowser title="Open in Browser" url={searchResult.link} />
-            )}
+            {searchResult.link && <Action.OpenInBrowser title="Open in Browser" url={searchResult.link} />}
             <Action.Push
               title="Edit link"
               icon={Icon.Pencil}
@@ -114,7 +133,14 @@ function SearchListItem({ searchResult, onEdit }: { searchResult: SearchResult, 
   );
 }
 
-function EditForm(props: { rowid?: string, text?: string, link?: string, title?: string, shortcut?: string, onEdit: (text?: string) => void }) {
+function EditForm(props: {
+  rowid?: string;
+  text?: string;
+  link?: string;
+  title?: string;
+  shortcut?: string;
+  onEdit: (text?: string) => void;
+}) {
   const { rowid = "", text, link, title, shortcut, onEdit } = props;
 
   const { pop } = useNavigation();
@@ -130,10 +156,7 @@ function EditForm(props: { rowid?: string, text?: string, link?: string, title?:
     <Form
       actions={
         <ActionPanel>
-          <Action.SubmitForm
-            title="Save link"
-            onSubmit={handleSubmit}
-          />
+          <Action.SubmitForm title="Save link" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
@@ -142,38 +165,43 @@ function EditForm(props: { rowid?: string, text?: string, link?: string, title?:
       <Form.TextField id="title" title="Title" defaultValue={title ?? ""} info="Used for Markdown links" />
       <Form.TextField id="shortcut" title="Shortcut" defaultValue={shortcut ?? ""} />
     </Form>
-  )
+  );
 }
 
 function previewAndCopy(push: (view: JSX.Element) => void, searchResult: SearchResult) {
   Clipboard.copy(searchResult.link!); //eslint-disable-line @typescript-eslint/no-non-null-assertion
   showToast({ style: Toast.Style.Success, title: "Copied link to clipboard" });
-  push(<Detail markdown={`![](${searchResult.link})`} actions={
-    <ActionPanel>
-      <Action.Paste
-        title="Paste link"
-        icon={Icon.Clipboard}
-        shortcut={{ modifiers: ["cmd"], key: "p" }}
-        content={`${searchResult.link}`}
-      />
-      <Action.Paste
-        title="Paste markdown link"
-        icon={Icon.Clipboard}
-        shortcut={{ modifiers: ["cmd"], key: "o" }}
-        content={`[${searchResult.title}](${searchResult.link})`}
-      />
-      <Action.CopyToClipboard
-        title="Copy plain URL"
-        content={`${searchResult.link}`}
-        shortcut={{ modifiers: ["cmd"], key: "u" }}
-      />
-      <Action.CopyToClipboard
-        title="Copy markdown link"
-        content={`[${searchResult.title}](${searchResult.link})`}
-        shortcut={{ modifiers: ["cmd"], key: "l" }}
-      />
-    </ActionPanel>
-  } />)
+  push(
+    <Detail
+      markdown={`![](${searchResult.link})`}
+      actions={
+        <ActionPanel>
+          <Action.Paste
+            title="Paste link"
+            icon={Icon.Clipboard}
+            shortcut={{ modifiers: ["cmd"], key: "p" }}
+            content={`${searchResult.link}`}
+          />
+          <Action.Paste
+            title="Paste markdown link"
+            icon={Icon.Clipboard}
+            shortcut={{ modifiers: ["cmd"], key: "o" }}
+            content={`[${searchResult.title}](${searchResult.link})`}
+          />
+          <Action.CopyToClipboard
+            title="Copy plain URL"
+            content={`${searchResult.link}`}
+            shortcut={{ modifiers: ["cmd"], key: "u" }}
+          />
+          <Action.CopyToClipboard
+            title="Copy markdown link"
+            content={`[${searchResult.title}](${searchResult.link})`}
+            shortcut={{ modifiers: ["cmd"], key: "l" }}
+          />
+        </ActionPanel>
+      }
+    />
+  );
 }
 
 function useSearch() {
@@ -195,7 +223,7 @@ function useSearch() {
         setState(() => ({
           results,
           searchText,
-          isLoading: false
+          isLoading: false,
         }));
       } catch (error) {
         setState((oldState) => ({
@@ -225,31 +253,45 @@ function useSearch() {
 
 async function appendLink(rowid: string, text: string, link: string, title: string, shortcut: string) {
   if (rowid) {
-    const results = await exec(`sqlite3 "${getLinkFileName()}" "update blitlinks set text='${escape(text)}', link='${escape(link)}', title='${escape(title)}', shortcut='${escape(shortcut)}' where rowid=${rowid};"`);
-    console.log({ results })
+    const results = await exec(
+      `sqlite3 "${getLinkFileName()}" "update blitlinks set text='${escape(text)}', link='${escape(
+        link
+      )}', title='${escape(title)}', shortcut='${escape(shortcut)}' where rowid=${rowid};"`
+    );
+    console.log({ results });
     showToast({ style: Toast.Style.Success, title: "Link updated" });
   } else {
-    const results = await exec(`sqlite3 "${getLinkFileName()}" "insert into blitlinks(text, link, title, shortcut) values('${escape(text)}', '${escape(link)}', '${escape(title)}', '${escape(shortcut)}')"`);
-    console.log({ results })
+    const results = await exec(
+      `sqlite3 "${getLinkFileName()}" "insert into blitlinks(text, link, title, shortcut) values('${escape(
+        text
+      )}', '${escape(link)}', '${escape(title)}', '${escape(shortcut)}')"`
+    );
+    console.log({ results });
     showToast({ style: Toast.Style.Success, title: "Link saved" });
   }
 }
 
 async function deleteLink(rowid: string, onDelete: () => void) {
   const results = await exec(`sqlite3 "${getLinkFileName()}" "delete from blitlinks where rowid=${rowid};"`);
-  console.log({ results })
+  console.log({ results });
   showToast({ style: Toast.Style.Success, title: "Link deleted" });
   onDelete();
 }
 
 async function performSearch(query: string): Promise<SearchResult[]> {
-  let results = {} as { stdout?: string, stderr?: string };
+  let results = {} as { stdout?: string; stderr?: string };
 
   if (!query || !query.trim()) {
-    results = await exec(`sqlite3 "${getLinkFileName()}" -json "select rowid, text, link, title, shortcut from blitlinks order by rowid desc;"`);
+    results = await exec(
+      `sqlite3 "${getLinkFileName()}" -json "select rowid, text, link, title, shortcut from blitlinks order by rowid desc;"`
+    );
   } else {
     query = query.replace(/[^a-zA-Z0-9]/g, " ");
-    results = await exec(`sqlite3 "${getLinkFileName()}" -json "select rowid, text, link, title, shortcut from blitlinks where blitlinks match 'shortcut : ${escape(query)} OR ${escape(query)}*' order by rank"`);
+    results = await exec(
+      `sqlite3 "${getLinkFileName()}" -json "select rowid, text, link, title, shortcut from blitlinks where blitlinks match 'shortcut : ${escape(
+        query
+      )} OR ${escape(query)}*' order by rank"`
+    );
   }
   if (!results.stdout) return [];
 
